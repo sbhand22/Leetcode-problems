853a1e5eda77bf6dac13920edc1f58f79263eeab
Sai Bhandari <128556629+sbhand22@users.noreply.github.com>
2023-09-07 16:59:21 -0700

    Time: 139 ms (63.04%), Space: 21.4 MB (21.85%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,6 +1,6 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda i : i[0])
+        intervals.sort()
         res=[intervals[0]]
 
         for s,e in intervals:
